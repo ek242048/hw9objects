f1d90036f4864ce84cfc360fcda4ab6bf850e2d2
Kate <ek242048@yandex.ru>
2022-04-24 23:42:51 +0500

    я молодец!)))

---
+++ src/ru/skypro/hw9objects/Author.java
@@ -0,0 +1,19 @@
+package ru.skypro.hw9objects;
+
+public class Author {
+    private String authorFirstName;
+    private String authorSecondName;
+
+    public Author (String authorFirstName, String authorSecondName) {
+        this.authorFirstName = authorFirstName;
+        this.authorSecondName = authorSecondName;
+    }
+
+    public String getAuthorFirstName(){
+        return this.authorFirstName;
+    }
+
+    public String getAuthorSecondName () {
+        return this.authorSecondName;
+    }
+}
